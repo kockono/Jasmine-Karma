b70de3b29edd1559b73c319918b7dfcab19edd32
kockono <kockonoop@gmail.com>
2022-10-07 00:23:20 -0600

    NegativeInifty, PositiveInfinity

--- 01-unit-testing/app.spec.js
+++ 01-unit-testing/app.spec.js
@@ -5,3 +5,17 @@ const saludar = require('./app');
 it('La función saluda', () => {
   expect(saludar('Platzi')).toBe('Hola Platzi');
 });
+
+expect(x).toEqual(y)             // Verifica si ambos valores son iguales.
+expect(x).toBe(y)                // Verifica si ambos objetos son iguales.
+expect(x).toMatch(pattern)       // Verifica si el valor pertenece al patrón establecido.
+expect(x).toBeDefined()          // Verifica si el valor está definido.
+expect(x).toBeUndefined()        // Verifica si el valor es indefinido.
+expect(x).toBeNull()             // Verifica si el valor es nulo.
+expect(x).toBeTruthy()           // Verifica si el valor es verdadero.
+expect(x).toBeFalsy();           // Verifica si el valor es falso.
+expect(x).toContain(y)           // Verifica si el valor actual contiene el esperado.
+expect(x).toBeLessThan(y)        // Verifica si el valor actual es menor que el esperado.
+expect(x).toBeGreaterThan(y)     // Verifica si el valor actual es mayor que el esperado.
+expect(x).toBeNegativeInfinity() // Verifica si el valor actual es Infinity
+expect(x).toBePositiveInfinity() // Verifica si el valor actual es null
